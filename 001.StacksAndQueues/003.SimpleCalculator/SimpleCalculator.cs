ac6e85b4de78cdff25b664c3c79df52215bbaa07
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-18 21:30:06 +0200

    add

---
+++ 001.StacksAndQueues/003.SimpleCalculator/SimpleCalculator.cs
@@ -0,0 +1,26 @@
+﻿//Stack and queues - lab, Task 003
+//11.03.2024, 23:02
+
+string[] expression = Console.ReadLine().Split();
+
+Array.Reverse(expression);
+
+Stack<string> stack = new Stack<string>(expression);
+
+while (stack.Count > 1)
+{
+    int firstNumber = int.Parse(stack.Pop());
+    string opt = stack.Pop();
+    int secondNumber = int.Parse(stack.Pop());
+
+    if (opt == "+")
+    {
+        stack.Push((firstNumber + secondNumber).ToString());
+    }
+    else if (opt == "-")
+    {
+        stack.Push((firstNumber - secondNumber).ToString());
+    }
+}
+
+Console.WriteLine(stack.Pop());
