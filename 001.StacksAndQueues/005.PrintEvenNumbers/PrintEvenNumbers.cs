bd873220b62566f7b86f9df908d39293420673c8
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-18 21:30:39 +0200

    add

---
+++ 001.StacksAndQueues/005.PrintEvenNumbers/PrintEvenNumbers.cs
@@ -0,0 +1,23 @@
+﻿//Stacks and queues - lab, Task 005
+//18.03.2024, 20:27
+
+int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+Queue<int> queueNumbers = new Queue<int>();
+
+for (int i = 0; i < array.Length; i++)
+{
+    queueNumbers.Enqueue(array[i]);
+}
+
+int len = queueNumbers.Count;
+
+for (int i = 0; i < len; i++)
+{
+    int number = queueNumbers.Dequeue();
+    if (number % 2 == 0)
+    {
+        queueNumbers.Enqueue(number);
+    }
+}
+
+Console.WriteLine(string.Join(", ", queueNumbers));
