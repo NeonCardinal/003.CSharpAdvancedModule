2212ae0b714a0eb39889c0d10532aeef4e369416
NeonCardinal <nvnnaidenov@gmail.com>
2024-03-18 21:30:57 +0200

    add

---
+++ 001.StacksAndQueues/006.Supermarket/Supermarket.cs
@@ -0,0 +1,29 @@
+﻿//Stacks and queues - lab, Task 006
+//18.03.2024, 21:18
+
+Queue<string> names = new Queue<string>();
+
+while(true)
+{
+    string input = Console.ReadLine();
+
+    if(input == "End")
+    {
+        Console.WriteLine($"{names.Count} people remaining.");
+        break;
+    }
+    else
+    {
+        names.Enqueue(input);
+    }
+
+    if(input == "Paid")
+    {
+        foreach(var item in names)
+        {
+            Console.WriteLine(item);
+        }
+
+        names.Clear();
+    }
+}
