97fa0d21eac56e51f29a171f722d6ae100d965c2
NeonCardinal <nvnnaidenov@gmail.com>
2024-09-16 21:30:17 +0300

    add

---
+++ 002.StackAndQueueExercise/005.FashionBoutique/FashionBoutique.cs
@@ -0,0 +1,42 @@
+﻿//Stack and queue - exercise, Task 005
+//12.09.2024, 00:39
+
+Stack<int> box = new Stack<int>();
+int racksCnt = 0;
+
+int[] inputs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int rackCapacity = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < inputs.Length; i++)
+{
+    box.Push(inputs[i]);
+}
+
+
+
+while(box.Count > 0)
+{
+    int sum = 0;
+    int currentValue = box.Peek();
+
+    while(sum + currentValue < rackCapacity)
+    {
+        if(sum + currentValue < rackCapacity)
+        {
+            sum += box.Pop();
+        }
+
+        if(box.Count > 0)
+        {
+            currentValue = box.Peek();
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    racksCnt++;
+}
+
+Console.WriteLine(racksCnt);
